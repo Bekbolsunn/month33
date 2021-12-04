5ce20f83c2f794f4d119756c401b91f77b9c49fd
Beka <karataevbekbolsun@gmail.com>
2021-12-03 21:35:52 -1000

    first_hw

--- main/urls.py
+++ main/urls.py
@@ -20,4 +20,7 @@ from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.say_hello),
+    path('rand_num/', views.random1),
+    path('rand_lox', views.rand_choice)
+
 ]
